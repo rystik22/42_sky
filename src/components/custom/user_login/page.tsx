de98ff0a613cfba3371eaf8a1b521714227cd35b
rystik <157467592+rystik22@users.noreply.github.com>
2025-05-12 11:12:17 +0400

    feat: Enhance 42 login process with environment variable for client ID and loading state management

--- src/components/custom/user_login/page.tsx
+++ src/components/custom/user_login/page.tsx
@@ -23,7 +23,13 @@ export default function LoginPage() {
 
   const handleFortyTwoLogin = () => {
     setIsLoading(true)
+    // Use environment variable for client ID instead of hardcoding
     window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_42_UID}&redirect_uri=https%3A%2F%2F42sky.vercel.app%2Fauth%2Fcallback&response_type=code`;
+    
+    // Reset loading state if the redirect is blocked
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
   }
 
   return (
