004468cf945237fe70881008726b9ea96dc0c86d
rystik <157467592+rystik22@users.noreply.github.com>
2025-05-20 14:00:34 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/42_sky', // replace with your GitHub repo name
+  trailingSlash: true, // ensures proper routing for static export
 };
 
 export default nextConfig;
